72f4580b22c6efcedae7e05208294b1fd179deee
William Scott <scott.william.david@gmail.com>
2026-01-18 19:57:24 +0000

    Test Comment

--- src/main/java/com/willy/williesmod/WilliesModClient.java
+++ src/main/java/com/willy/williesmod/WilliesModClient.java
@@ -2,6 +2,7 @@ package com.willy.williesmod;
 
 import net.fabricmc.api.ClientModInitializer;
 
+// Very Important Comment
 public class WilliesModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
